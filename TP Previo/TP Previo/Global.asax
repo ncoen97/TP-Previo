c90b2cdafc6555e62bb1d8a7a1a9186af8fc3700
Nicolas <ncoen97@gmail.com>
2018-04-22 14:47:54 -0300

    login añadido

--- TP Previo/TP Previo/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TP_Previo.MvcApplication" Language="C#" %>
---
+++ TP-Previo/TP-Previo-2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TP_Previo_2.Startup))]
+namespace TP_Previo_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
